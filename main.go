c131bcd133d5a2d0be439afcaeeed69e46dc73f1
hefju <hefju@qq.com>
2014-02-27 23:21:30 +0800

    Signed-off-by: hefju <hefju@qq.com>

--- main.go
+++ main.go
@@ -1,19 +1,75 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"github.com/lunny/godbc"
-	 "github.com/lunny/xorm"
+	_ "github.com/bmizerany/pq"
 )
 
-type User struct { Id int64 Name string `xorm:"varchar(25) not null unique 'usr_name'"` } 
-
 func main() {
+	db, err := sql.Open("postgres", "user=postgres password=1 dbname=posts sslmode=disable")
+	checkErr(err)
 
-	engine, err = xorm.NewEngine("MsSql", "SERVER=127.0.0.1;UID=sa;PWD=123;DATABASE=DemoDB")
-	defer engine.Close()
+	//插入数据
+	stmt, err := db.Prepare("INSERT INTO userinfo(username,departname,created) VALUES($1,$2,$3) RETURNING uid")
+	checkErr(err)
 
-	user := new(User)
-user.Name = "myname"
-affcted, err := engine.Insert(user)
+	res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
+	checkErr(err)
+
+	//pg不支持这个函数，因为他没有类似MySQL的自增ID
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	fmt.Println(id)
+
+	//更新数据
+	stmt, err = db.Prepare("update userinfo set username=$1 where uid=$2")
+	checkErr(err)
+
+	res, err = stmt.Exec("astaxieupdate", 1)
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	//查询数据
+	rows, err := db.Query("SELECT * FROM userinfo")
+	checkErr(err)
+
+	for rows.Next() {
+		var uid int
+		var username string
+		var department string
+		var created string
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println(uid)
+		fmt.Println(username)
+		fmt.Println(department)
+		fmt.Println(created)
+	}
+
+	//删除数据
+	stmt, err = db.Prepare("delete from userinfo where uid=$1")
+	checkErr(err)
+
+	res, err = stmt.Exec(1)
+	checkErr(err)
+
+	affect, err = res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	db.Close()
+
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
